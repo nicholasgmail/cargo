2d94536bd06f975e0ebf0ad8e542a1b201372590
Nicholas Us <nicholas.us.03@gmail.com>
2025-09-10 12:43:51 +0300

    update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -33,7 +33,7 @@ export default defineNuxtConfig({
             ],
             noscript: [
                 // <noscript>JavaScript is required</noscript>
-                {textContent: 'JavaScript is required'}
+                {textContent: 'cargo'}
             ]
         },
     },
